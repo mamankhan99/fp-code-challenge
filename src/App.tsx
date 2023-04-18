810e92d158d601cf546fc021ec077e8c52d681d7
Maman Khan <41908571+mamankhan99@users.noreply.github.com>
2023-04-18 01:31:08 +0100

    fetch data from moke api in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Metrics } from './api/types';
+import getMetricsData from './api';
+
 function App() {
+  const [data, setData] = useState<Metrics[] | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getMetricsData();
+      setData(response.data);
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Data</h1>
+      {data &&
+        data.map((item: Metrics) => (
+          <div key={item.id}>
+            <p>{item.label}</p>
+            <p>{item.category}</p>
+            <p>{item.description}</p>
+            <p>{item.type}</p>
+            <p>{item.value}</p>
+          </div>
+        ))}
     </div>
   );
 }
